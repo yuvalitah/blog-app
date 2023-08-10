b5025e77c795de7d1be698d5577548f508a5f9da
Yuval Itah <yuval18998@gmail.com>
2023-08-10 13:03:44 +0300

    make connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      database: 'blog',
+      username: 'root',
+      password: 'mysql123',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      migrations: ['dist/migrations/*.{ts,js}'],
+      migrationsTableName: 'typeorm_migrations',
+      logger: 'simple-console',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
