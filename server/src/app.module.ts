82ad1bb8d456211691a4dfb5c91b40c3599ec104
Yuval Itah <yuval18998@gmail.com>
2023-08-11 14:31:00 +0300

    change

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -13,7 +13,7 @@ import { ApiModule } from './api/api.module';
       database: 'blog',
       username: 'root',
       password: 'mysql123',
-      entities: ['dist/**/*.entity.{ts,js}'],
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       migrations: ['dist/migrations/*.{ts,js}'],
       migrationsTableName: 'typeorm_migrations',
       logger: 'simple-console',
