72fbefbd1858228f61dc7fbe420c7e0b69c766c5
Yuval Itah <yuval18998@gmail.com>
2023-08-21 15:11:25 +0300

    revert last commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const config: ConfigService = app.get(ConfigService);
+  const port: number = config.get<number>('PORT');
+
   app.enableCors();
-  await app.listen(3001);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(port, () => {
+    console.log('[WEB]', config.get<string>('BASE_URL'));
+  });
 }
+
 bootstrap();
